94245111fc12504870d9ce885d6360259248bf8f
Arun K Bharathan <arunkbharathan@gmail.com>
2020-04-02 13:30:52 +0530

    push first then find median

--- Chapter11/findRunningMedian.go
+++ Chapter11/findRunningMedian.go
@@ -53,13 +53,11 @@ func streamingMedian() {
 			// else the avg of mid 2 elements split sorted array to 2 equal half
 			if cnt%2 == 0 {
 				if floatVal > preMedian {
+
+					heap.Push(minH, floatVal)
+
 					rVal := heap.Pop(minH).(float64)
 					median = (rVal + preMedian) / 2
-					if floatVal < median {
-						heap.Push(maxH, floatVal)
-					} else {
-						heap.Push(minH, floatVal)
-					}
 					if rVal < median {
 						heap.Push(maxH, rVal)
 					} else {
@@ -73,47 +71,39 @@ func streamingMedian() {
 
 				}
 				if floatVal < preMedian {
+					heap.Push(maxH, floatVal)
 					lVal := heap.Pop(maxH).(float64)
 					median = (lVal + preMedian) / 2
-					if floatVal < median {
-						heap.Push(maxH, floatVal)
-					} else {
-						heap.Push(minH, floatVal)
-					}
 					if lVal < median {
 						heap.Push(maxH, lVal)
 					} else {
 						heap.Push(minH, lVal)
 					}
-					if preMedian < median {
-						heap.Push(maxH, preMedian)
-					} else {
-						heap.Push(minH, preMedian)
-					}
 
+				}
+				if floatVal == median {
+					heap.Push(minH, floatVal)
 				}
 				fmt.Println(median)
 				preMedian = median
 			} else {
 				if floatVal > preMedian {
+
+					heap.Push(minH, floatVal)
+
 					rVal := heap.Pop(minH).(float64)
 					median = rVal
-					if floatVal < median {
-						heap.Push(maxH, floatVal)
-					} else {
-						heap.Push(minH, floatVal)
-					}
 
 				}
 				if floatVal < median {
+					heap.Push(maxH, floatVal)
+
 					lVal := heap.Pop(maxH).(float64)
 					median = lVal
-					if floatVal < median {
-						heap.Push(maxH, floatVal)
-					} else {
-						heap.Push(minH, floatVal)
-					}
 
+				}
+				if floatVal == median {
+					heap.Push(minH, floatVal)
 				}
 				fmt.Println(median)
 				preMedian = median
