f3cad2db1c3f349c4c6e96faf89338d9d6d93372
Arun K Bharathan <arunkbharathan@gmail.com>
2020-04-11 18:04:49 +0530

    rearrange

--- Chapter15/main.go
+++ Chapter15/main.go
@@ -14,11 +14,11 @@ func main() {
 
 func buildMinimumHeightBSTFromSortedArray() {
 	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	buildBSTAVL(arr)
+	buildBSTRedBlack(arr)
 	result := buildMinimumHeightBST(arr)
 	fmt.Println(result)
 
-	buildBSTAVL(arr)
-	buildBSTRedBlack(arr)
 }
 
 // func findMostVisitedPage() {
