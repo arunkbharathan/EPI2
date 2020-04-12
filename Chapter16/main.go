289319336c7ce1476d6a7a64dae94a47c370dfd7
Arun K Bharathan <arunkbharathan@gmail.com>
2020-04-12 10:18:58 +0530

    recursion start

---
+++ Chapter16/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	towerOfHanoi()
+}
+
+func towerOfHanoi() {
+	disks := 3
+	solveTowerOfHanoi(disks)
+}
---
+++ Chapter16/solveTowerOfHanoi.go
@@ -0,0 +1,5 @@
+package main
+
+func solveTowerOfHanoi(disks int) {
+
+}
