cdce7e676270567497d6274253539c11d54a9ada
Arun K Bharathan <arunkbharathan@gmail.com>
2020-04-12 14:14:47 +0530

    tower of hanoi

--- Chapter16/solveTowerOfHanoi.go
+++ Chapter16/solveTowerOfHanoi.go
@@ -1,5 +1,34 @@
 package main
 
-func solveTowerOfHanoi(disks int) {
+import (
+	"fmt"
+
+	"github.com/golang-collections/collections/stack"
+)
+
+// https://www.mathsisfun.com/games/towerofhanoi.html
+func solveTowerOfHanoi(disksToMove int) {
+	towers := map[string]*stack.Stack{
+		"A": stack.New(),
+		"B": stack.New(),
+		"C": stack.New(),
+	}
+	for i := disksToMove; i > 0; i-- {
+		towers["A"].Push(i)
+	}
+	totalMoves := 0
+	hanoiRecurssion(disksToMove, towers, "A", "C", "B", &totalMoves)
+	fmt.Println("Total moves: ", totalMoves)
+}
+
+func hanoiRecurssion(disksToMove int, towers map[string]*stack.Stack, from, to, using string, totalMoves *int) {
+	if disksToMove > 0 {
+		hanoiRecurssion(disksToMove-1, towers, from, using, to, totalMoves)
+		val := towers[from].Pop().(int)
+		towers[to].Push(val)
+		fmt.Printf("moving %d from  %s to %s\n", val, from, to)
+		*totalMoves++
+		hanoiRecurssion(disksToMove-1, towers, using, to, from, totalMoves)
+	}
 
 }
