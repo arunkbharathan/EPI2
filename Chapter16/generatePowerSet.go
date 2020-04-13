d8f53df3b9d27c9422ef3edc50c5153e68062824
Arun K Bharathan <arunkbharathan@gmail.com>
2020-04-13 13:26:00 +0530

    power set

---
+++ Chapter16/generatePowerSet.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func powerSet(arr []int) {
+	currSet := []int{}
+	generatePowerSet(arr, 0, currSet)
+}
+
+func generatePowerSet(arr []int, ind int, currSet []int) {
+	if ind == len(arr) {
+		fmt.Println(currSet)
+		return
+	}
+	currSet2 := make([]int, 0, len(arr))
+	currSet2 = append(currSet2, currSet...)
+	currSet2 = append(currSet2, arr[ind])
+	generatePowerSet(arr, ind+1, currSet2)
+	generatePowerSet(arr, ind+1, currSet)
+
+}
--- Chapter16/main.go
+++ Chapter16/main.go
@@ -1,20 +1,24 @@
 package main
 
-import "fmt"
-
 func main() {
 	// towerOfHanoi()
 	// nQueens()
-	printPermutations()
+	// printPermutations()
+	printPowerset()
 }
 
-func printPermutations() {
-	arr := []int{2, 3, 5, 7}
-	result := generatePermutations(arr)
-	for _, val := range result {
-		fmt.Println(val)
-	}
+func printPowerset() {
+	arr := []int{0, 1, 2}
+	powerSet(arr)
 }
+
+// func printPermutations() {
+// 	arr := []int{2, 3, 5, 7}
+// 	result := generatePermutations(arr)
+// 	for _, val := range result {
+// 		fmt.Println(val)
+// 	}
+// }
 
 // func nQueens() {
 // 	size := 2.0
