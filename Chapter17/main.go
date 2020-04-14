d0dc4683585448c1a2dd19a6d2528835d6b14358
Arun K Bharathan <arunkbharathan@gmail.com>
2020-04-15 00:44:23 +0530

    knapsack

--- Chapter17/main.go
+++ Chapter17/main.go
@@ -1,25 +1,28 @@
 package main
 
-import "fmt"
-
 func main() {
 	// countNumberOfScoreCombinations()
 	// editDistance()
 	// traverse2DArray()
-	searchForASequenceIn2DArray()
+	// searchForASequenceIn2DArray()
+	solveKnapSackProblem()
 }
 
-func searchForASequenceIn2DArray() {
-	seq := []int{1, 3, 4, 6}
-	grid := [][]int{
-		{1, 2, 3},
-		{3, 4, 5},
-		{5, 6, 7},
-	}
-
-	fmt.Printf("%v occurs in %v :%t\n", seq, grid, search2DArray(seq, grid))
+func solveKnapSackProblem() {
 
 }
+
+// func searchForASequenceIn2DArray() {
+// 	seq := []int{1, 3, 4, 6}
+// 	grid := [][]int{
+// 		{1, 2, 3},
+// 		{3, 4, 5},
+// 		{5, 6, 7},
+// 	}
+
+// 	fmt.Printf("%v occurs in %v :%t\n", seq, grid, search2DArray(seq, grid))
+
+// }
 
 // func traverse2DArray() {
 // 	arraySize := 5
