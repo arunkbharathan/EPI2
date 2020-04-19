b50ec0529f122eae31f5498ee85eba8381c737e7
Arun K Bharathan <arunkbharathan@gmail.com>
2020-04-19 10:20:24 +0530

    a bit more remaining

--- Chapter19/transformOneStringToAnother.go
+++ Chapter19/transformOneStringToAnother.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
+type nodeData struct {
+	word  string
+	count int
+}
+
 func transformOneStringToAnother(dist map[string]struct{}, str1, str2 string) int {
+	queue := []nodeData{{str1, 0}}
+
+	for len(queue) > 0 {
+		strData := queue[0]
+		queue = queue[1:]
+		strBytes := []byte(strData.word)
+		for i := 0; i < len(strBytes); i++ {
+			for j := 0; j < 26; j++ {
+				strBytes[i] = 'a' + byte(j)
+				fmt.Println(string(strBytes))
+			}
+			strBytes = []byte(strData.word)
+		}
+	}
 	return -1
 }
